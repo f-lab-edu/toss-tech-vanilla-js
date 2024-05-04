f2bd944b8f5111ed841a512d28e94461a72d4ab8
yoonsu <110542210+sossost@users.noreply.github.com>
2024-05-04 19:16:15 +0900

    test : createRouter 테스트 추가

--- __test__/router/router.test.js
+++ __test__/router/router.test.js
@@ -1,6 +1,7 @@
 import { extractUrlParams } from "../../src/router";
+import createRouter from "../../src/router";
 
-describe("extractUrlParams", () => {
+describe("extractUrlParams function test", () => {
   test("returns an empty object if no params are specified", () => {
     const route = {
       params: [],
@@ -19,5 +20,43 @@ describe("extractUrlParams", () => {
     expect(extractUrlParams(route, pathname)).toEqual({
       articleId: "react-native-2024",
     });
+  });
+});
+
+describe("createRouter", () => {
+  let router;
+  let mockCallback;
+  let mockNotFoundCallback;
+
+  beforeEach(() => {
+    router = createRouter();
+    mockCallback = jest.fn();
+    mockNotFoundCallback = jest.fn();
+    router.addRoute("/article/:id", mockCallback);
+    router.setNotFound(mockNotFoundCallback);
+
+    global.window = Object.create(window);
+    const url = "http://toss.tech";
+    Object.defineProperty(window, "location", {
+      value: {
+        href: url,
+        pathname: url,
+      },
+      writable: true,
+    });
+  });
+
+  test("calls the callback when the route matches", () => {
+    window.location.pathname = "/article/react-native-2024";
+    router.start();
+
+    expect(mockCallback).toHaveBeenCalled();
+  });
+
+  test("calls notFound when no route matches", () => {
+    window.location.pathname = "/not-added-page";
+    router.start();
+
+    expect(mockNotFoundCallback).toHaveBeenCalled();
   });
 });
