ff0230e1b597e8b584da3dd47f04fa090380aee3
yoonsu <110542210+sossost@users.noreply.github.com>
2024-05-05 17:19:55 +0900

    feat : article-list, article-list-item entry에 추가

--- src/components/entry.js
+++ src/components/entry.js
@@ -1,5 +1,4 @@
-import Header from "./layout/header.js";
-import Footer from "./layout/footer.js";
-
-window.customElements.define("main-header", Header);
-window.customElements.define("main-footer", Footer);
+export { default as Header } from "./layout/header.js";
+export { default as Footer } from "./layout/footer.js";
+export { default as ArticleListItem } from "./article/ArticleListItem.js";
+export { default as ArticleList } from "./article/ArticleList.js";
