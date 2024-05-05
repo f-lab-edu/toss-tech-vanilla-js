56aceda874b831ec18797ecc4c8ded6e7701f08d
yoonsu <110542210+sossost@users.noreply.github.com>
2024-05-05 21:05:21 +0900

    feat : article-detail-page 구현

---
+++ src/pages/ArticleDetailPage.js
@@ -0,0 +1,57 @@
+import { CUSMTOM_ELEMENTS_NAME } from "../constants/customElementName.js";
+import Component from "../components/Component.js";
+import { getArticleById } from "../apis/getArticle.js";
+
+class ArticleDetailPage extends Component {
+  constructor() {
+    super(["article"]);
+  }
+
+  async connectedCallback() {
+    const articleId = this.getAttribute("articleId");
+    const article = await getArticleById(articleId);
+    this.props = article;
+
+    this.render();
+  }
+
+  get styles() {
+    return `
+      <style>
+        .article-detail-page__container {
+            max-width: 700px;
+            margin: 0 auto;
+        }
+        .article-detail__container {
+            width: 92%;
+            margin: 0 auto;
+            margin-bottom: 80px;
+      </style>
+    `;
+  }
+
+  createHTML(article) {
+    return `
+      <div class='article-detail-page__container'>
+        <div class="article-detail__container">
+            <${CUSMTOM_ELEMENTS_NAME.ARTICLE_DETAIL}
+                thumbnail="${article.thumbnail}"
+                title="${article.title}"
+                profileImage="${article.profile_image}"
+                author="${article.author}"
+                position="${article.position}"
+                date="${article.date}"
+                content="${article.content}"
+            </${CUSMTOM_ELEMENTS_NAME.ARTICLE_DETAIL}>
+        </div>
+      </div>
+    `;
+  }
+}
+
+window.customElements.define(
+  CUSMTOM_ELEMENTS_NAME.ARTICLE_DETAIL_PAGE,
+  ArticleDetailPage
+);
+
+export default ArticleDetailPage;
