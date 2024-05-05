6732545f8c7e78742ef2a4a90498a02d0b7e47d2
yoonsu <110542210+sossost@users.noreply.github.com>
2024-05-05 17:18:34 +0900

    feat : article-list-item 컴포넌트 구현

---
+++ src/components/article/ArticleListItem.js
@@ -0,0 +1,100 @@
+import Component from "../Component.js";
+
+export default class ArticleListItem extends Component {
+  constructor() {
+    super(["id", "title", "description", "date", "thumbnail"]);
+  }
+
+  get styles() {
+    return `
+    <style>
+        .article {
+            display: flex;
+            align-items: center;
+            cursor: pointer;
+            text-decoration: none;
+            margin-bottom: var(--article-space);
+            color: black;
+        }
+        .article__thumbnail {
+          width: 240px;
+          height: 240px;
+          object-fit: cover;
+          border-radius: 14px;
+          margin-right: 48px;
+          -webkit-transition: 0.2s ease-in-out;
+          transition: 0.2s ease-in-out;
+      }
+        .article:hover .article__thumbnail {
+            transform: translate3d(0, -3%, 0);
+            box-shadow: 0 12px 24px 0 rgba(0, 0, 0, 0.15);
+        }
+        .article__content {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            height: 100%;
+            line-height: 1.6;
+        }
+        .article__title {
+            display: inline-block;
+            margin-top: 0;
+            margin-bottom: 14px;
+            overflow: hidden;
+            font-size: 36px;
+            font-weight: 700;
+            line-height: 1.4;
+            color: #333d4b;
+            text-overflow: ellipsis;
+            word-break: keep-all;
+            overflow-wrap: break-word;
+            -webkit-transition: color 0.3s ease;
+            transition: color 0.3s ease;
+            -webkit-line-clamp: 2;
+        }
+        .article:hover .article__title {
+          color: #3182f6;
+        }
+        .article__description {
+            display: block;
+            margin: 0;
+            margin-bottom: 12px;
+            font-size: 17px;
+            line-height: 1.6;
+            color: #4e5968;
+            text-overflow: ellipsis;
+            -webkit-line-clamp: 2;
+        }
+        .article__date {
+            line-height: 1.6;
+            color: #8b95a1;
+            margin: 0;
+            font-size: 15px;
+        }
+    </style>
+    `;
+  }
+
+  createHTML({ id, title, description, date, thumbnail }) {
+    return `
+      <a
+        class='article'
+        href='/article/${id}'
+        data-navigate='/article/${id}'
+      >
+        <img
+          class='article__thumbnail'
+          srcset='${thumbnail}'
+          alt='${title} 썸네일 이미지'
+        />
+        <div class='article__content'>
+          <h3 class="article__title">${title}</h3>
+          <p class="article__description">${description}</p>
+          <p class="article__date">${date}</p>
+        </div>
+      </a>
+    `;
+  }
+}
+
+window.customElements.define("article-list-item", ArticleListItem);
