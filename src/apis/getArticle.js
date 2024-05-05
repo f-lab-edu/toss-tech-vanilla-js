f17fe1e47d53093544704a9fdeb568206d7afcb6
yoonsu <110542210+sossost@users.noreply.github.com>
2024-05-05 21:04:43 +0900

    feat : getArticle 파일 추가

---
+++ src/apis/getArticle.js
@@ -0,0 +1,17 @@
+const BASE_URL = window.location.origin;
+
+export const getArticleById = async (articleId) => {
+  try {
+    const response = await fetch(`${BASE_URL}/data/article.json`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+    return data[articleId];
+  } catch (error) {
+    console.error(error);
+  }
+};
