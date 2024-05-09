de74b156a17d0a32e91233d9c233d15363486144
ynnsuis <shagrat@stoa-investment.com>
2024-05-09 20:26:52 +0900

    feat : article.d.ts 파일 추가

---
+++ src/types/article.d.ts
@@ -0,0 +1,11 @@
+export type Category = "tech" | "design";
+
+export type Article = {
+  id: string;
+  title: string;
+  content: string;
+  thumbnail: string;
+  author: string;
+  description: string;
+  date: string;
+};
