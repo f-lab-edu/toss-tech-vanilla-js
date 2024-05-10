b1e8fb8edf648def527641486cd74e71cbd4fcd8
ynnsuis <shagrat@stoa-investment.com>
2024-05-10 14:50:52 +0900

    feat : type 추가 및 articleId 없을 시 예외처리 추가

--- src/pages/ArticleDetailPage.ts
+++ src/pages/ArticleDetailPage.ts
@@ -1,6 +1,17 @@
-import { CUSTOM_ELEMENTS_NAME } from "../constants/customElementName.js";
-import Component from "../components/Component.js";
-import { getArticleById } from "../apis/getArticle.js";
+import { CUSTOM_ELEMENTS_NAME } from "../constants/customElementName";
+import Component from "../components/Component";
+import { getArticleById } from "../apis/getArticle";
+import createRouter from "../router";
+
+type ArticleDetailPageProps = {
+  thumbnail: string;
+  title: string;
+  profile_image: string;
+  author: string;
+  position: string;
+  date: string;
+  content: string;
+};
 
 class ArticleDetailPage extends Component {
   constructor() {
@@ -8,7 +19,13 @@ class ArticleDetailPage extends Component {
   }
 
   async connectedCallback() {
+    const router = createRouter();
     const articleId = this.getAttribute("articleId");
+    if (!articleId) {
+      router.navigate("/404");
+      return;
+    }
+
     const article = await getArticleById(articleId);
     this.props = article;
 
@@ -30,7 +47,7 @@ class ArticleDetailPage extends Component {
     `;
   }
 
-  createHTML(article) {
+  createHTML(article: ArticleDetailPageProps) {
     return `
       <div class='article-detail-page__container'>
         <div class="article-detail__container">
