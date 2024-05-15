b587f86acb564e9995dd5b03a14d1b871b6d8270
Joonho Kim <joonhok@smartfitnow.com>
2024-05-15 17:18:28 +0900

    index.js 파일 index.ts 로 변경

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import "./components/entry.js";
 import createRouter from "./router.js";
 import createPages from "./pages/index.js";
+import "./styles/reset.css";
+import "./styles/global.css";
 
 const container = document.querySelector("main");
 const pages = createPages(container);
