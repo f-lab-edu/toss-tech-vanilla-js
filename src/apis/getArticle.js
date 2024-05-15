78a25f14c34b4bfc02be4f7215678cb969414884
Joonho Kim <joonhok@smartfitnow.com>
2024-05-15 17:19:03 +0900

    json 데이터 fetch 대신 import json 로 로드

--- src/apis/getArticle.js
+++ src/apis/getArticle.js
@@ -1,16 +1,26 @@
-const BASE_URL = window.location.origin;
+// const BASE_URL = window.location.origin;
+
+// export const getArticleById = async (articleId) => {
+//   try {
+//     const response = await fetch(`${BASE_URL}/data/article.json`, {
+//       method: "GET",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     });
+
+//     const data = await response.json();
+//     return data[articleId];
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+import articles from "../data/article.json";
 
 export const getArticleById = async (articleId) => {
   try {
-    const response = await fetch(`${BASE_URL}/data/article.json`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    const data = await response.json();
-    return data[articleId];
+    return articles[articleId];
   } catch (error) {
     console.error(error);
   }
--- src/apis/getArticleList.js
+++ src/apis/getArticleList.js
@@ -1,19 +1,34 @@
-const BASE_URL = window.location.origin;
+// const BASE_URL = window.location.origin;
+
+// export const getArticleListByCategory = async (category) => {
+//   try {
+//     const response = await fetch(
+//       `${BASE_URL}/data/${category}ArticleList.json`,
+//       {
+//         method: "GET",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       }
+//     );
+
+//     const data = await response.json();
+//     return data.articles;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+import techJson from "../data/techArticleList.json";
+import designJson from "../data/designArticleList.json";
 
 export const getArticleListByCategory = async (category) => {
   try {
-    const response = await fetch(
-      `${BASE_URL}/data/${category}ArticleList.json`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-
-    const data = await response.json();
-    return data.articles;
+    if (category === "tech") {
+      return techJson.articles;
+    } else if (category === "design") {
+      return designJson.articles;
+    }
   } catch (error) {
     console.error(error);
   }
