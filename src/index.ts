83d692ba6028222857fecf11e00a158be484d2d4
jang_yoonsu <shagrat8546@gmail.com>
2024-05-19 19:11:22 +0900

    fix : 헤더, 푸터 누락된 추가 로직 추가

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import "./components/entry";
 import createRouter from "./router";
 import createPages from "./pages";
+import { CUSTOM_ELEMENTS_NAME } from "./constants/customElementName";
 
 const app = document.querySelector("#app");
 const container = document.createElement("main");
+const header = document.createElement(CUSTOM_ELEMENTS_NAME.APP_HEADER);
+const footer = document.createElement(CUSTOM_ELEMENTS_NAME.APP_FOOTER);
+app!.appendChild(header!);
 app!.appendChild(container!);
+app!.appendChild(footer!);
 
 const pages = createPages(container!);
 const router = createRouter();
